830c480adab50a5996f0b67704d8841453796fa2
¨Niv <bn1702kodehode@gmail.com>
2025-03-27 11:17:31 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Customisert_God_Morgen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime date = DateTime.Now;
+
+        double time = date.ToOADate();
+
+        if (time >= 12.00)
+        {
+            Console.WriteLine($"Good morning!");
+        }
+        else if (time <= 20.00)
+        {
+            Console.WriteLine($"Good evening!");
+        }
+        else
+        {
+            Console.WriteLine($"Good afternoon");
+        }
+
+        Console.WriteLine ($"The time is {date:T}!");
+    }
+}
